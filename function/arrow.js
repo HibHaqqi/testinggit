8988cf78982b07178b8ba727023154cb70ea750e
hibhaqqi <haqqi09@gmail.com>
2023-06-21 19:27:03 +0800

    functionarrow

---
+++ function/arrow.js
@@ -0,0 +1,12 @@
+function diskon(x){
+    let musimPanemik = (x*30)/100;
+    return musimPanemik
+}
+
+let sale = diskon(200000)
+console.log(sale);
+
+hello = () => {
+    return "hello";
+}
+console.log.apply(hello);
