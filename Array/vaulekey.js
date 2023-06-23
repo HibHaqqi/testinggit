6e5979b14a8149cc81289afb5fdb5fcd103b82db
hibhaqqi <haqqi09@gmail.com>
2023-06-23 16:18:33 +0800

    object

---
+++ Array/vaulekey.js
@@ -0,0 +1,10 @@
+/** const student = new Object();
+student.name = "budi";
+student.age = 25;
+
+console.log(student);
+**/
+
+const student = Object.assign({name : "budi", age: 25});
+
+console.log(student);
