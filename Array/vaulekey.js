ad807fb1eed610ba96e3f7f4a882faff0a6af615
hibhaqqi <haqqi09@gmail.com>
2023-06-23 16:37:56 +0800

    object

--- Array/vaulekey.js
+++ Array/vaulekey.js
@@ -4,7 +4,17 @@ student.age = 25;
 
 console.log(student);
 **/
-
+/**
 const student = Object.assign({name : "budi", age: 25});
 
 console.log(student);
+**/
+class studentnew{
+constructor(name,age){
+    this.nama = name;
+    this.age = age;
+}
+}
+
+const newStudent = new studentnew("budo",30);
+console.log(newStudent);
